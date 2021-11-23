b9a8eeec877a1ea08d901457c50f419de8a530b2
Burak Mustafa Dündar <86871573+bmdundar24@users.noreply.github.com>
2021-11-23 22:33:55 +0300

    Update signup.js

--- signup.js
+++ signup.js
@@ -2,7 +2,7 @@ const loading = document.getElementById("loading");
 const submitBtn = document.getElementById("submit");
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
-window.onload = () => { 
+window.onload = () => {  
     setTimeout(fillFormInput, 500);
 }
 
